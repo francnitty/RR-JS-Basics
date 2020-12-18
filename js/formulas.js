e1507df67ef4064f9ecf817c22d9216c5a9e101c
Franco M. Thomas <74632584+francnitty@users.noreply.github.com>
2020-12-17 22:29:58 -0500

    maad formulas

--- js/formulas.js
+++ js/formulas.js
@@ -2,79 +2,153 @@
 function addition(num1, num2){
   return -1;
 }
+function add(num1, num2){
+  let result = num1 + num2;
+  return result}
+
 
 function subtraction(num1, num2){
   return -1;
 }
+function sub(num1, num2){
+  let answer = num1 - num2;
+  return answer}
 
 function multiplication(num1, num2){
   return -1;
 }
+function multiplication(num1, num2){
+  let result = num1 * num2;
+  return result}
 
 function division(num1, num2){
   return -1;
 }
+function division(num1, num2){
+  let haase = num1 / num2;
+  return haase}
 
 // Area formulaas
 function areaSquare(side){
   return -1;
 }
+function areasquare(side){
+  let oggenfuss = Math.pow(side, 2);
+  return oggenfuss}
 
 function areaRectangle(length, width){
   return -1;
+}
+function areaRectangle(length, width){
+  let fletcher = length * width;
+    return fletcher
 }
 
 function areaParallelogram(base, height){
   return -1;
 }
+function areaParallelogram(base, height){
+  let cooper = base * height;
+  return cooper
+}
 
 function areaTriangle(base, height){
   return -1;
+}
+function areaTriangle(base, height){
+  let fraiger = base * height;
+  return fraiger
 }
 
 function Circle(radius){
   return -1;
 }
+function Circle(radius){
+  let montclair = Math.pow(3.14, radius, 2);
+  return montclair
+}
 
 function Sphere(radius){
   return -1;
+}
+function Sphere(radius){
+  let savage = 4 * 3.14 * Math.pow(radius, 2);
+  return savage
 }
 
 // Surface Area formulas
 function surfaceAreaCube(side){
   return -1;
 }
+function surfaceAreaCube(side){
+  let kayan = 6 * (side * side);
+  return kayan
+}
 
 function surfaceAreaCylinder(radius, height){
   return -1;
+}
+function surfaceAreaCylinder(radius, height){
+  let ishola = 2 * 3.14 * radius * height;
+  return ishola
 }
 
 // Perimeter formulas
 function perimeterSquare(side){
   return -1;
 }
+function perimeterSquare(side){
+  let chaunte = Math.pow(4, 2)
+  return chaunte
+}
 
-function perimeterRectangle(length, height){
+function perimeterRectangle(length, width){
   return -1;
+}
+function perimeterRectangle(length, width){
+  let tyler = (2 * length) + (2 * width);
+  return tyler
 }
 
 function perimeterTriangle(side1, side2, side3){
   return -1;
 }
+function perimeterTriangle(side1, side2, side3){
+  let rhonda = side1 + side2 + side3;
+  return rhonda
+}
 
 function perimeterCircle(diameter){
   return -1;
+}
+function perimeterCircle(diameter){
+  let darnley = Math.PI * diameter
+  return darnley
 }
 
 // Volume formulas
 function volumeCube(side){
   return -1;
 }
+function volumeCube(side){
+  let jayden = Math.pow(side, 3);
+  return jayden
+}
 
 function volumeRectangular(length, width, height){
   return -1;
 }
+function volumeRectangular(length, width, height){
+  let mia = length * width * height;
+  return mia
+}
 
 function volumeCylinder(radius, height){
   return -1;
+}
+function volumeCylinder(radius, height){
+  let brooklynn = Math.PI * (radius * radius) * height;
+  return brooklynn
+
+  }
 }
